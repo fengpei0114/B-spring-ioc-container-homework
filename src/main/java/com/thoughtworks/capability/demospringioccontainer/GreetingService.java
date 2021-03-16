f060f07933b7672f88c7a83fdb71d583fcedad0b
fengpei0114 <feng_pei0114@163.com>
2021-03-16 23:19:33 +0800

    finish p2s2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,10 +1,10 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public class GreetingService {
 
+    public GreetingService() {
+        System.out.println("GreetingService is created");
+    }
     String sayHi() {
         return "hello world";
     }
