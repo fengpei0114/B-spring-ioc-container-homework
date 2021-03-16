0e3febea8279043c4a716ff0df7c7fcf0e56095f
fengpei0114 <feng_pei0114@163.com>
2021-03-16 23:28:15 +0800

    finish p3s2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,13 +1,16 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 
 @Component
 public class Foo {
 
     private Bar bar;
 
-    public Foo(Bar bar) {
+    @Autowired
+    public Foo(@Lazy Bar bar) {
         this.bar = bar;
     }
 
